9187a934802f1cb6642580c852a174da22b07ada
Matteo Gobbi <job@matteogobbi.it>
2014-05-01 17:11:57 +0100

    Small change

--- MGNotificationCenterExample/MGSecondViewController.m
+++ MGNotificationCenterExample/MGSecondViewController.m
@@ -20,19 +20,16 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    [[MGNotificationCenter defaultCenter] addObserver:self selector:nil name:@"name1" object:nil];
-    [[MGNotificationCenter defaultCenter] addObserver:self selector:nil name:@"name2" object:nil];
-    
     NSUInteger count = [[MGNotificationCenter defaultCenter] count];
-    NSUInteger countForName = [[MGNotificationCenter defaultCenter] countForName:@"name1"];
-    
     NSLog(@"Count: %lu", (unsigned long)count);
+    
+    NSUInteger countForName = [[MGNotificationCenter defaultCenter] countForName:@"name1"];
     NSLog(@"CountForName: %lu", (unsigned long)countForName);
     
     NSArray *names = [[MGNotificationCenter defaultCenter] allNames];
-    NSArray *namesForObserver = [[MGNotificationCenter defaultCenter] allNamesForObserver:self];
-    
     NSLog(@"Names: %@", names);
+    
+    NSArray *namesForObserver = [[MGNotificationCenter defaultCenter] allNamesForObserver:self];
     NSLog(@"NamesForObserver: %@", namesForObserver);
 }
 
